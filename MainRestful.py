962cec95f8592fa13187988c11535705088a5942
MikeTim <merickson@millenniumbrooklynhs.org>
2015-07-22 09:53:08 -0400

    disaplys page but broke code

--- MainRestful.py
+++ MainRestful.py
@@ -1,21 +1,36 @@
 from flask import Flask, render_template, request
-import urllib
-#!/usr/bin/python
+import urllib2
+import json
+
 
 MainRestful = Flask(__name__)
 
-@MainRestful.route('/')
+@MainRestful.route('/main')
 def main():
-	data = urllib.urlopen('https://api.wheretheiss.at/v1/satellites/25544')
-	data2 = data.read()
-	data.close
-	return data2	
+	#Opens File v
+	data1 = urllib2.open('https://api.wheretheiss.at/v1/satellites/25544')
+	#print data
+	data2 = json.loads(data1)
+	#data2 = data.read()
+	#data.close
 	lat = data2['latitude']
 	long = data2['longitude']
-	location = lat + " " + long
-	#return json.loads(data)
+	location = str(lat + " " + long)
 	return location
-	
+
+
+@MainRestful.route('/result')
+def result():
+	return render_template("index.html")
+
+#DEBUGGING PAGE !!!( TESTING )!!!
+@MainRestful.route('/testing')
+def testpage():
+	return location
+
+
+
+
 if __name__ == '__main__':
 	MainRestful.run()
 	MainRestful.debug = True
